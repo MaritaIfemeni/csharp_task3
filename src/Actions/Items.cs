cd4f0ef8edc829022cab08b1135d9da09f3c1d2b
MaritaIfemeni <marita.ifemeni@gmail.com>
2023-06-01 13:08:04 +0300

    initial commit

---
+++ src/Actions/Items.cs
@@ -0,0 +1,7 @@
+namespace src.Actions
+{
+    public class Items
+    {
+        
+    }
+}
---
+++ src/Actions/Printer.cs
@@ -0,0 +1,10 @@
+namespace src.Actions
+{
+    public class Printer
+    {
+        public static void Print(string message)
+        {
+            System.Console.WriteLine(message);
+        }
+    }
+}
